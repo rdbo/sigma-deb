0099952870bff48531f04dc9ea388617c5c750ed
rdbo <rdbodev@gmail.com>
2022-10-15 11:10:58 -0300

    automated build process

--- build.sh
+++ build.sh
@@ -3,26 +3,6 @@
 # Build Dependencies:
 # fakeroot checkinstall
 
-# CheckInstall Menu
-# This package will be built according to these values: 
-#
-# 0 -  Maintainer: [ Rdbo ]
-# 1 -  Summary: [ Sigma Linux configuration for Debian ]
-# 2 -  Name:    [ sigma-linux ]
-# 3 -  Version: [ 1.0.0 ]
-# 4 -  Release: [ 0 ]
-# 5 -  License: [ GPL ]
-# 6 -  Group:   [ checkinstall ]
-# 7 -  Architecture: [ amd64 ]
-# 8 -  Source location: [ sigma+linux-1.0.0 ]
-# 9 -  Alternate source location: [  ]
-# 10 - Requires: [ libx11-dev, libxft-dev, libxinerama-dev, libxrandr-dev, libimlib2-dev, libharfbuzz-dev, libfreetype-dev, gcc ]
-# 11 - Recommends: [ xinit, dunst, libnotify-bin, feh, net-tools, vim, firefox ]
-# 12 - Suggests: [  ]
-# 13 - Provides: [ sigma-linux ]
-# 14 - Conflicts: [  ]
-# 15 - Replaces: [  ]
-
 ROOTDIR="$(pwd)"
 VERSION="1.0.0"
 
@@ -31,7 +11,22 @@ mkdir -p bin deb
 cp -Lr src deb/sigma+linux-$VERSION
 
 cd deb/sigma+linux-$VERSION
-fakeroot checkinstall --install=no
+fakeroot checkinstall \
+    -y \
+    -D \
+    --install=no \
+    --maintainer="Rdbo <rdbo at GitHub>" \
+    --pkgname="sigma-linux" \
+    --pkgversion="$VERSION" \
+    --pkgrelease="0" \
+    --pkglicense="AGPLv3" \
+    --pkggroup "checkinstall" \
+    # --pkgarch="amd64" \
+    --pkgsource="sigma+linux-$VERSION" \
+    --requires="libx11-dev, libxft-dev, libxinerama-dev, libxrandr-dev, libimlib2-dev, libharfbuzz-dev, libfreetype-dev, gcc" \
+    --recommends="xinit, dunst, libnotify-bin, feh, vim, firefox" \
+    --provides="sigma-linux"
+
 mv *.deb "$ROOTDIR"/bin
 cd "$ROOTDIR"
 rm -rf deb
