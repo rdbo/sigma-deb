762121723c1158244fff454b18f745f4039bb311
rdbo <rdbodev@gmail.com>
2022-10-16 21:52:27 -0300

    organized packages by creating separate files

--- build.sh
+++ build.sh
@@ -6,6 +6,13 @@
 ROOTDIR="$(pwd)"
 VERSION="1.0.0"
 
+read_list() {
+	grep -E '^[^#\n]' "$1" | sed 's/#.*//g;s/\s//g' | sed -z 's/\n/, /g' | rev | cut -c 3- | rev
+}
+
+required="$(read_list required.list)"
+recommended="$(read_list recommended.list)"
+
 rm -rf bin deb
 mkdir -p bin deb
 
@@ -23,8 +30,8 @@ fakeroot checkinstall \
     --pkglicense="AGPLv3" \
     --pkggroup "checkinstall" \
     --pkgsource="sigma+linux-$VERSION" \
-    --requires="libx11-dev, libxft-dev, libxinerama-dev, libxrandr-dev, libimlib2-dev, libharfbuzz-dev, libfreetype-dev, libxtst-dev, gcc" \
-    --recommends="xinit, xrandr, dunst, libnotify-bin, feh, vim, maim, man, pulseaudio, pulseaudio-utils, papirus-icon-theme, adwaita-icon-theme, firefox-esr, gimp, inkscape, libreoffice-gnome, ssh, rsync, screen, git, wget, curl, perl, gawk, sed, coreutils, g++, make, cmake, build-essentials, checkinstall, fakeroot, live-build, python3, python3-pip, openjdk-11-jdk, openjdk-17-jdk, tor, torbrowser-launcher, weechat, tar, gzip, xz-utils, p7zip-full, p7zip-rar, ntfs-3g, lxappearance, pulsemixer, wireguard-tools, openvpn, openssl, gnupg, iwd, poppler-utils, imagemagick, mpv, apache2, libapache2-mod-php, postgresql, mariadb-server, mariadb-client, keepassxc, x11-xkb-utils, x11-xserver-utils, x11-utils, shotcut, chromium, qemu-system, ovmf, blender, bluez, bluez-tools, obs-studio, audacity, xfe" \
+    --requires="$required" \
+    --recommends="$recommended" \
     --provides="sigma-linux"
 
 mv *.deb "$ROOTDIR"/bin
