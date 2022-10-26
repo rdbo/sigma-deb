d7ff30f6dcdb9ec3f1144946f7b2a277668467f4
rdbo <rdbodev@gmail.com>
2022-10-25 21:29:30 -0300

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 git submodule update --remote --merge
 cd src
-rm dwm dwmblocks-async dmenu st slock rootfs
+rm dwm dwmblocks-async dmenu st slock
 ln -s ../sigma-dwm ./dwm
 ln -s ../sigma-dwmblocks-async ./dwmblocks-async
 ln -s ../sigma-dmenu ./dmenu
 ln -s ../sigma-st ./st
 ln -s ../sigma-slock ./slock
-ln -s ../rootfs ./rootfs
 cd ..
